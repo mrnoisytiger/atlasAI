a383f03af0d9c88302003dd24876a1cedf68bdca
Felix Jen <fjen@felixjen.com>
2016-12-22 17:27:32 -0800

    Removed contents of development file

--- api/php_info.php
+++ api/php_info.php
@@ -4,10 +4,10 @@
 	
 	//phpinfo( INFO_ALL );
 	
-	require 'functions/slackOutput.php';
+	// require 'functions/slackOutput.php';
 	
-	$user_id = $_POST['user_id'];
+	// $user_id = $_POST['user_id'];
 
-	echo slackOutput($user_id, "text");
+	// echo slackOutput($user_id, "text");
 	
 ?>
